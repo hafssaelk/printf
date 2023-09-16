cf30b7b00045aa921c577a745671db266ac039ca
hafssa elkoi <hafssa.elkoi0@gmail.com>
2023-09-16 05:53:55 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,20 +1,49 @@
 #include "main.h"
-#include <string.h>
-int _printf(const char *format, ...)
-{
-    int size;
-    va_list args;
+
+void custom_print_buffer(char buffer[], int *buff_ind);
+
+int custom_printf(const char *format, ...) {
+    int i, printed = 0, printed_chars = 0;
+    int flags, width, precision, size, buff_ind = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
 
     if (format == NULL)
         return (-1);
 
-    size = strlen(format);
-    if (size <= 0)
-        return (0);
+    va_start(list, format);
 
-    va_start(args, format);
-    size = handler(format, args);
-    va_end(args);
+    for (i = 0; format && format[i] != '\0'; i++) {
+        if (format[i] != '%') {
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                custom_print_buffer(buffer, &buff_ind);
+            printed_chars++;
+        } else {
+            custom_print_buffer(buffer, &buff_ind);
+            flags = get_flags(format, &i);
+            width = get_width(format, &i, list);
+            precision = get_precision(format, &i, list);
+            size = get_size(format, &i);
+            ++i;
+            printed = custom_handle_print(format, &i, list, buffer,
+                flags, width, precision, size);
+            if (printed == -1)
+                return (-1);
+            printed_chars += printed;
+        }
+    }
 
-    return (size);
+    custom_print_buffer(buffer, &buff_ind);
+
+    va_end(list);
+
+    return (printed_chars);
+}
+
+void custom_print_buffer(char buffer[], int *buff_ind) {
+    if (*buff_ind > 0)
+        write(1, &buffer[0], *buff_ind);
+
+    *buff_ind = 0;
 }
