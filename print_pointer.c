0112f29d2d7ec31034e4b935e5c14f3badc04078
simobls <belassalsimo78@gmail.com>
2023-09-17 20:44:20 +0100

    add

--- print_pointer.c
+++ print_pointer.c
@@ -7,39 +7,34 @@
  */
 int print_pointer(va_list list)
 {
-char *var;
-int count = 0, i;
-char *str;
+	char *var;
+	int count = 0, i;
+	char *str;
 
-var = itoa(va_arg(list, unsigned long int), 16);
+	var = va_arg(list, unsigned long int);
 
-if (var == NULL)
-return (-1);
+	if (var == NULL)
+		return (-1);
 
-if (strcmp(var, "0") == 0)
-{
-str = "(nil)";
-for (i = 0; str[i] != '\0'; ++i)
-{
-_putchar(str[i]);
-count++;
-}
-return (count);
-}
-else
-{
-str = "0x";
-for (i = 0; str[i] != '\0'; ++i)
-{
-_putchar(str[i]);
-count++;
-}
-for (i = 0; var[i] != '\0'; ++i)
-{
-_putchar(var[i]);
-count++;
-}
-}
-
-return (count);
+	if (strcmp(var, "0") == 0)
+	{
+		str = "(nil)";
+		for (i = 0; str[i] != '\0'; ++i)
+		{
+			_putchar(str[i]);
+			count++;
+		}
+		return (count);
+	}
+	else
+	{
+		str = "0x";
+		for (i = 0; str[i] != '\0'; ++i)
+		{
+		_putchar(str[i]);
+		count++;
+		}
+		count += print_hexadecimal_low(var);
+	}
+	return (count);
 }
