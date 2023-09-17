17e1e0dc38f2c3183688be88fba38c8745237809
simobls <belassalsimo78@gmail.com>
2023-09-17 20:37:19 +0100

    add

--- _printf.c
+++ _printf.c
@@ -1,20 +1,30 @@
 #include "main.h"
 #include <string.h>
+
+/**
+ * _printf - Produces output according to a format...
+ * @format: Is a character string. The format string...
+ * is composed of zero or more directives
+ *
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
+
 int _printf(const char * const format, ...)
 {
-    int size;
-    va_list args;
+	int size;
+	va_list args;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    size = strlen(format);
-    if (size <= 0)
-        return (0);
+	size = strlen(format);
+	if (size <= 0)
+		return (0);
 
-    va_start(args, format);
-    size = handler(format, args);
-    va_end(args);
+	va_start(args, format);
+	size = handler(format, args);
+	va_end(args);
 
-    return (size);
+	return (size);
 }
