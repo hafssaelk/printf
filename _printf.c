bb3a2b5667bbfe9134460e4dabf2b72e00b0a559
hafssa elkoi <hafssa.elkoi0@gmail.com>
2023-09-19 08:04:09 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...);
-
-int main() {
-    int num = 42;
-    char ch = 'A';
-    char *str = "Hello, World!";
-
-    int result = _printf("This is a number: %d\nThis is a character: %c\nThis is a string: %s\n", num, ch, str);
-
-    printf("\nTotal characters printed: %d\n", result);
-
-    return 0;
-}
-
 int _printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
     
-    int count = 0;
+    int count = 0; // To keep track of the number of characters printed
     
     while (*format != '\0') {
         if (*format == '%') {
-            format++;
+            format++; // Move past the '%'
             switch (*format) {
                 case 'c': {
                     char c = va_arg(args, int);
@@ -32,7 +18,7 @@ int _printf(const char *format, ...) {
                     break;
                 }
                 case 's': {
-                    char *s = __builtin_va_arg(args,char *);
+                    char *s = va_arg(args, char *);
                     while (*s != '\0') {
                         putchar(*s);
                         s++;
@@ -45,18 +31,32 @@ int _printf(const char *format, ...) {
                     count++;
                     break;
                 default:
+                    // Unsupported format specifier, just print it as is
                     putchar('%');
                     putchar(*format);
                     count += 2;
             }
         } else {
+            // Non-format character, just print it
             putchar(*format);
             count++;
         }
-        format++;
+        format++; // Move to the next character in the format string
     }
     
     va_end(args);
     return count;
+}
+
+int main() {
+    int num = 42;
+    char ch = 'A';
+    char *str = "Hello, World!";
+    
+    int result = _printf("This is a number: %d\nThis is a character: %c\nThis is a string: %s\n", num, ch, str);
+    
+    printf("\nTotal characters printed: %d\n", result);
+    
+    return 0;
 }
 
