199ebbb0a5483472b274a3c82ae783aa70037543
hafssa elkoi <hafssa.elkoi0@gmail.com>
2023-09-19 09:44:45 -0700

    task 5

---
+++ custom_printf.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void custom_printf(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    while (*format) {
+        if (*format == '%') {
+            format++; // Move past '%'
+            if (*format == 'S') {
+                format++; // Move past 'S'
+                const char* str = va_arg(args, const char*);
+
+                while (*str) {
+                    if (*str >= 32 && *str < 127) {
+                        putchar(*str);
+                    } else {
+                        printf("\\x%02X", *str);
+                    }
+                    str++;
+                }
+            } else {
+                putchar('%'); // Print '%' if the specifier is not recognized
+            }
+        } else {
+            putchar(*format); // Print normal characters
+        }
+        format++;
+    }
+
+    va_end(args);
+}
+
+int main() {
+    const char* text = "Hello, \x01\x02\x03 World!";
+    custom_printf("Custom format: %S\n", text);
+    return 0;
+}
+
