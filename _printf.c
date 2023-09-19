dd40437995ca57669f1bbb0ba8d6eb7fe267b11b
hafssa elkoi <hafssa.elkoi0@gmail.com>
2023-09-19 08:20:12 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int _printf(const char *format, ...);
+
+int main() {
+    int num = 42;
+    char ch = 'A';
+    char *str = "Hello, World!";
+
+    int result = _printf("This is a number: %d\nThis is a character: %c\nThis is a string: %s\n", num, ch, str);
+
+    printf("\nTotal characters printed: %d\n", result);
+
+    return 0;
+}
+
 int _printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
     
-    int count = 0; // To keep track of the number of characters printed
+    int count = 0;
     
     while (*format != '\0') {
         if (*format == '%') {
-            format++; // Move past the '%'
+            format++;
             switch (*format) {
                 case 'c': {
                     char c = va_arg(args, int);
@@ -19,10 +33,15 @@ int _printf(const char *format, ...) {
                 }
                 case 's': {
                     char *s = va_arg(args, char *);
-                    while (*s != '\0') {
-                        putchar(*s);
-                        s++;
-                        count++;
+                    if (s != NULL) {
+                        while (*s != '\0') {
+                            putchar(*s);
+                            s++;
+                            count++;
+                        }
+                    } else {
+                        // Handle the case when s is NULL (optional).
+                        // You can decide how to handle this case based on your requirements.
                     }
                     break;
                 }
@@ -31,32 +50,18 @@ int _printf(const char *format, ...) {
                     count++;
                     break;
                 default:
-                    // Unsupported format specifier, just print it as is
                     putchar('%');
                     putchar(*format);
                     count += 2;
             }
         } else {
-            // Non-format character, just print it
             putchar(*format);
             count++;
         }
-        format++; // Move to the next character in the format string
+        format++;
     }
     
     va_end(args);
     return count;
-}
-
-int main() {
-    int num = 42;
-    char ch = 'A';
-    char *str = "Hello, World!";
-    
-    int result = _printf("This is a number: %d\nThis is a character: %c\nThis is a string: %s\n", num, ch, str);
-    
-    printf("\nTotal characters printed: %d\n", result);
-    
-    return 0;
 }
 
